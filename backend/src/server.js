f92a67bb6a7ae265119e2d24b2fbecf721d2f2cb
Patrick Benedict Lirio <pblirio.1doc@gmail.com>
2025-11-05 09:35:20 +0800

    backend structure

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+// const express = require("express"); //for normal js
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+const app = express();
+
+app.use("/api/notes", notesRoutes);
+
+app.listen(5001, () => {
+  console.log("Server is running on Port 5001");
+});
