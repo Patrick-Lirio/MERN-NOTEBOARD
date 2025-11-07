e18d8b3d012e1ed32b8e583422b216c9044c16d5
Patrick Benedict Lirio <pblirio.1doc@gmail.com>
2025-11-07 22:55:33 +0800

    added rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,28 @@ import express from "express";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 // console.log(process.env.MONGO_URI);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middleware
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
-app.use(({ req, res, next }) => {
-  console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
-  next();
-});
+// simple custom middleware
+// app.use(({ req, res, next }) => {
+//   console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
+//   next();
+// });
+
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on Port: ", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on Port: ", PORT);
+  });
 });
